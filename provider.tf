efcb67b66145d3bc67856d52f260494a2df4cc14
charmingruby <mrrubycanal3105@gmail.com>
2024-12-31 17:25:58 -0300

    feat: base setup with s3 be

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "1.10.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    region = "us-east-1"
+    bucket = "asapp-tf"
+    key    = "backend/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = var.aws-region
+}
